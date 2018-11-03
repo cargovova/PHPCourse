c2f5e7ca06498408b47e34967418dc17aa6da4bd
vova <vova@vova-PC>
2018-11-03 12:49:53 +0400

    Crating structure. Free abstraction levels. 1. Management Engine. 2. Management Cylinders. 3. Other.

--- HomeWorks/hw_5.php
+++ HomeWorks/hw_5.php
@@ -1,3 +1,38 @@
 <?php
-echo phpinfo();
+// Впуск
+function induction()
+{
+    return "Открыть впускной клапан, опустить поршень.";
+}
+// Сжатие
+function compression()
+{
+    return "Поднять поршень.";
+}
+// Рабочий ход
+function power()
+{
+    return "Искра, взрыв, опустить поршень";
+}
+// Выхлоп
+function exhaust()
+{
+    return "Открыть выпускной клапан, поднять поршень.";
+}
+// Управление цилиндрами
+function management_cylinders()
+{
+    $induction = induction();
+    $compression = compression();
+    $power = power();
+    $exhaust = exhaust();
+    return $managementCylinders;
+}
+// Управление двигателем
+function management_engine()
+{
+    $managementCylinders = management_cylinders();
+    return $managementCylinders;
+}
+echo management_engine();
 ?>
