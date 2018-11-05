757a694c0d2153bcad93eb872457afbde31f14f9
vova <vova@vova-PC>
2018-11-05 12:32:08 +0400

    Task "Engine" is done. Abstraction levels: 1. Management engine. 2. Management tacts. 3. Engine tact.

--- HomeWorks/hw_5.php
+++ HomeWorks/hw_5.php
@@ -1,38 +1,77 @@
 <?php
+
 // Впуск
 function induction()
 {
-    return "Открыть впускной клапан, опустить поршень.";
+    return "Открыть впускные клапана, опустить поршень.";
 }
+
 // Сжатие
 function compression()
 {
     return "Поднять поршень.";
 }
+
 // Рабочий ход
 function power()
 {
     return "Искра, взрыв, опустить поршень";
 }
+
 // Выхлоп
 function exhaust()
 {
-    return "Открыть выпускной клапан, поднять поршень.";
+    return "Открыть выпускные клапана, поднять поршень.";
 }
+// Функция для любого такта двигателя
+function engine_tact(
+    $tactFirstCylinder,
+    $tactSecondCylinder,
+    $tactThirdCylinder,
+    $tactFourthCylinder)
+{
+    $cylinders = [
+        "Поршень 1: $tactFirstCylinder",
+        "Поршень 2: $tactSecondCylinder",
+        "Поршень 3: $tactThirdCylinder",
+        "Поршень 4: $tactFourthCylinder"
+    ];
+    return $cylinders;
+}
+
 // Управление цилиндрами
-function management_cylinders()
+function management_tacts()
 {
     $induction = induction();
     $compression = compression();
     $power = power();
     $exhaust = exhaust();
-    return $managementCylinders;
+
+    $managementTacts = [
+        engine_tact($induction, $compression, $exhaust, $power),
+        engine_tact($compression, $power, $induction, $exhaust),
+        engine_tact($power, $exhaust, $compression, $induction),
+        engine_tact($exhaust, $induction, $power, $compression)
+    ];
+    return $managementTacts;
 }
+
 // Управление двигателем
 function management_engine()
 {
-    $managementCylinders = management_cylinders();
-    return $managementCylinders;
+    $managementTacts = management_tacts();
+    return $managementTacts;
 }
-echo management_engine();
+
+// Вывод (При запуске в консоли будет отображаться работа двигателя, IDE падает).
+$i = 1;
+while ($i == 1) {
+    foreach (management_engine() as $value) {
+        foreach ($value as $index){
+            echo $index;
+            echo "\n";
+        }
+        echo "\n";
+    }
+}
 ?>
